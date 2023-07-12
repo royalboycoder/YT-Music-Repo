db6d8ab9c41abf6e7e6e3c1e5fd65188df30df57
Royal_Boy_Amit <116754209+royalboycoder@users.noreply.github.com>
2023-07-12 15:12:18 +0530

    Create __main__.py

---
+++ Royalkifeelings/Callmusic/__main__.py
@@ -0,0 +1,23 @@
+import asyncio
+import importlib
+
+from pytgcalls import idle
+
+from Royalkifeelings.import BOT_USERNAME, bot, call_py
+from Royalkifeelings.plugins import ALL_PLUGINS
+
+loop = asyncio.get_event_loop()
+
+
+async def Royalboyamit_boot():
+    for all_module in ALL_PLUGINS:
+        importlib.import_module("Royalkifeelings.plugins." + all_module)
+    await bot.start()
+    await call_py.start()
+    await idle()
+    print(f"ɢᴏᴏᴅʙʏᴇ!\nStopping @{BOT_USERNAME}")
+    await bot.stop()
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(Royalboyamit_boot())
