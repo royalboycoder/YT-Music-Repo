dade20e5b82b46a702c4eb83e75a5a92a3cd7b1c
ExoticHero <126850195+ExoticHero@users.noreply.github.com>
2023-07-14 23:50:34 +0530

    Fixed

--- Royalkifeelings/__main__.py
+++ Royalkifeelings/__main__.py
@@ -1,7 +1,8 @@
 import asyncio
 import importlib
 from pytgcalls import idle
-from Royalkifeelings.callmusic.config import BOT_USERNAME, bot, call_py
+from Royalkifeelings.callmusic.config import BOT_USERNAME
+from Royalkifeelings import bot, call_py
 from Royalkifeelings.plugins import ALL_PLUGINS
 
 
