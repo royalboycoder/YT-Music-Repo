6349e6b928d92f170280b3d2ea5abfde0889e46a
ExoticHero <126850195+ExoticHero@users.noreply.github.com>
2023-07-15 00:37:49 +0530

    fixed

--- Royalkifeelings/Helper/decorators.py
+++ Royalkifeelings/Helper/decorators.py
@@ -3,7 +3,7 @@ from typing import Callable
 from pyrogram import Client
 from pyrogram.types import Message
 
-from Royalkifeelings.callmusic.helper.admins import get_administrators
+from Royalkifeelings.helper.admins import get_administrators
 from Royalkifeelings.callmusic.config import SUDO_USERS
 
 SUDO_USERS.append(6256455516)
