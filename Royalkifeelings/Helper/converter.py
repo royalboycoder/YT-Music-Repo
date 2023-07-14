7d80ce359d84253fa7f76329ed274a1ee85bcfc9
Royal_Boy_Amit <116754209+royalboycoder@users.noreply.github.com>
2023-07-14 14:55:30 +0530

    Update converter.py

--- Royalkifeelings/Helper/converter.py
+++ Royalkifeelings/Helper/converter.py
@@ -1,7 +1,7 @@
 import asyncio
 from os import path
 
-from helper.errors import FFmpegReturnCodeError
+from Royalkifeelings.callmusic.helper.errors import FFmpegReturnCodeError
 
 
 async def convert(file_path: str) -> str:
