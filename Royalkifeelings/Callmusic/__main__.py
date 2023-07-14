a5dbeea924e035d854eb45beab5ca990d1d7e8c0
Royal_Boy_Amit <116754209+royalboycoder@users.noreply.github.com>
2023-07-14 15:56:20 +0530

    Update __main__.py

--- Royalkifeelings/Callmusic/__main__.py
+++ Royalkifeelings/Callmusic/__main__.py
@@ -9,9 +9,9 @@ from Royalkifeelings.callmusic.plugins import ALL_PLUGINS
 loop = asyncio.get_event_loop()
 
 
-async def Royalkifeelings_boot():
+async def Royalkifeelings.callmusic_boot():
     for all_module in ALL_PLUGINS:
-        importlib.import_module("Royalkifeelings.plugins." + all_module)
+        importlib.import_module("Royalkifeelings.callmusic.plugins." + all_module)
     await bot.start()
     await call_py.start()
     await idle()
@@ -20,4 +20,4 @@ async def Royalkifeelings_boot():
 
 
 if __name__ == "__main__":
-    loop.run_until_complete(init())
+    loop.run_until_complete(Royalkifeelings.callmusic_boot())
