6166f470bad69d01f6ec555e7311e7ffb51cce2b
ExoticHero <126850195+ExoticHero@users.noreply.github.com>
2023-07-17 00:18:42 +0530

    Update start.py

--- Royalkifeelings/plugins/start.py
+++ Royalkifeelings/plugins/start.py
@@ -29,8 +29,9 @@ async def _human_time_duration(seconds):
 
 @Royalboyamit.on_message(filters.command(["start", "help"]) & ~filters.group)
 async def start(_, message: Message):
-    await message.reply_text(
-        f"""**𝐇𝐞𝐥𝐥𝐨 {BOT_NAME}✨ 𝐓𝐡𝐢𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 🎶 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 🥀 𝐕𝐩𝐬 💫𝐒𝐞𝐫𝐯𝐞𝐫 🌎 𝐅𝐞𝐞𝐥 ❤️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐕𝐜 😎🤟
+    await message.reply_photo(
+        photo=f"",
+        caption=f"""**𝐇𝐞𝐥𝐥𝐨 {BOT_NAME}✨ 𝐓𝐡𝐢𝐬 𝐀𝐝𝐯𝐚𝐧𝐜𝐞 🥀 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐌𝐮𝐬𝐢𝐜 🎶 𝐁𝐨𝐭 𝐑𝐮𝐧 𝐎𝐧 𝐏𝐫𝐢𝐯𝐚𝐭𝐞 🥀 𝐕𝐩𝐬 💫𝐒𝐞𝐫𝐯𝐞𝐫 🌎 𝐅𝐞𝐞𝐥 ❤️ 𝐇𝐢𝐠𝐡 𝐐𝐮𝐚𝐥𝐢𝐭𝐲 𝐌𝐮𝐬𝐢𝐜 🎧 𝐈𝐧 𝐕𝐜 😎🤟
 📡 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲: [𝐋σⱱəᴙ 𝐖σᴙɭɗ 𝐂ɦɑʈʈɪŋʛ 𝐇ʋB](https://t.me/royalkifeelings) 💞...**""",
      reply_markup=InlineKeyboardMarkup(
             [
