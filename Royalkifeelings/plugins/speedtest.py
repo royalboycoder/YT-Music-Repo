46490050f321842f02bb1681fd793229a0f2e267
Royal_Boy_Amit <116754209+royalboycoder@users.noreply.github.com>
2023-07-18 00:43:39 +0530

    Update speedtest.py

--- Royalkifeelings/plugins/speedtest.py
+++ Royalkifeelings/plugins/speedtest.py
@@ -23,7 +23,7 @@ def testspeed(m):
     return result
 
 
-@Royalboyamit.on_message(command("speedtest"))
+@Royalboyamit.on_message(command("sp"))
 @sudo_users_only
 async def speedtest_function(Royalboyamit: Royalboyamit, message: Message):
     m = await message.reply_text("❤")
