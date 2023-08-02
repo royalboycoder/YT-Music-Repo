ef66db04aff4b2116c3063f05969cf4e2eadc607
Royal_Boy_Amit <116754209+royalboycoder@users.noreply.github.com>
2023-08-02 21:30:44 +0530

    Update broadcast.py

--- Royalkifeelings/plugins/broadcast.py
+++ Royalkifeelings/plugins/broadcast.py
@@ -6,7 +6,7 @@ from Royalkifeelings.callmusic.config import OWNER_ID
 from Royalkifeelings import bot, Royalboyamit
 
 
-@bot.on_message(filters.command("broadcast") & filters.user(OWNER_ID))
+@Royalboyamit.on_message(filters.command("broadcast") & filters.user(OWNER_ID))
 async def broadcast(_, message: Message):
     if not message.reply_to_message:
         pass
@@ -46,63 +46,4 @@ async def broadcast(_, message: Message):
         except Exception:
             pass
     await message.reply_text(f"✅ Broadcast complete in {sent} Group.")
-
-
-@bot.on_message(filters.command("broadcast_pin") & filters.user(OWNER_ID))
-async def broadcast(_, message: Message):
-    if not message.reply_to_message:
-        pass
-    else:
-        x = message.reply_to_message.message_id
-        y = message.chat.id
-        sent = 0
-        pin = 0
-        chats = []
-        schats = await get_served_chats()
-        for chat in schats:
-            chats.append(int(chat["chat_id"]))
-        for i in chats:
-            try:
-                m = await c.forward_messages(i, y, x)
-                try:
-                    await m.pin(disable_notification=True)
-                    pin += 1
-                except Exception:
-                    pass
-                await asyncio.sleep(0.3)
-                sent += 1
-            except Exception:
-                pass
-        await message.reply_text(
-            f"✅ Broadcast complete in {sent} Group.\n📌 Sent with {pin} chat pins."
-        )
-        return
-    if len(message.command) < 2:
-        await message.reply_text(
-            "**usage**:\n\n/broadcast_pin (`message`) or (`reply to message`)"
-        )
-        return
-    text = message.text.split(None, 1)[1]
-    sent = 0
-    pin = 0
-    chats = []
-    schats = await get_served_chats()
-    for chat in schats:
-        chats.append(int(chat["chat_id"]))
-    for i in chats:
-        try:
-            m = await c.send_message(i, text=text)
-            try:
-                await m.pin(disable_notification=True)
-                pin += 1
-            except Exception:
-                pass
-            await asyncio.sleep(0.3)
-            sent += 1
-        except Exception:
-            pass
-    await message.reply_text(
-        f"✅ Broadcast completed in {sent} Group.\n📌 Sent with {pin} chat pins."
-    )
-
 
