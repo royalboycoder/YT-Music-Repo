4f51e9d17e64208d3084a57edcb6ea789630ecf3
coixo <vivansingh8372@outlook.com>
2025-01-17 17:25:02 +0530

    [MINOR-FIX]

--- Royalkifeelings/__init__.py
+++ Royalkifeelings/__init__.py
@@ -1,19 +1,20 @@
-from pyrogram import Client
+from pyrogram import Client, filters
 from pytgcalls import PyTgCalls, idle
 from Royalkifeelings.callmusic.config import API_HASH, API_ID, BOT_TOKEN, OWNER_ID, SESSION_NAME
 from Royalkifeelings.plugins import ALL_PLUGINS
 
 bot = Client(
-    "Royalkifeelings",
+    name="Royalkifeelings",
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
 )
 
 Royalboyamit = Client(
+    name="Yt", 
     api_id=API_ID,
     api_hash=API_HASH,
-    session_name=SESSION_NAME,
+    session_string=SESSION_NAME,
 )
 
 user = PyTgCalls(
